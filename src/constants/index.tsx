7094270afdca51ca2b9d80f99039730ec7dd9890
kehindeoluseyi18 <kehindeoluseyi18@gmail.com>
2023-07-05 08:02:55 +0100

    schoolmoni naviagtion

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -22,9 +22,9 @@ interface NavigationInterface {
 
 export const navigationLinks: Array<NavigationInterface> = [
   { title: "What we do", pathname: "what-we-do", scrollOffset: -50 },
-  { title: "Our Impact", pathname: "/", scrollOffset: -100 },
-  { title: "Sponsor", pathname: "/", scrollOffset: -70 },
+  { title: "SchoolMoni", pathname: "/schoolmoni", scrollOffset: -70 },
   { title: "About us", pathname: "/aboutUs", scrollOffset: -70 },
+  { title: "Our Impact", pathname: "/", scrollOffset: -100 },
   { title: "How it works", pathname: "/", scrollOffset: -40 },
 ];
 
